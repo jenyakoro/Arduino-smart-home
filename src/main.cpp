5c345f1482566263776d428fe2fb0fa7a77f820d
Eugene Korotaev <jenyakoro@mail.ru>
2024-03-31 16:32:06 +0300

    remove humidity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,63 @@
 #include <Arduino.h>
-//call the relevant library file
 #include <Servo.h>
-//#include <Wire.h>
-#include <LCD_1602_RUS.h>  // подключаем библиотеку LCD_1602_RUS
+#include <LCD_1602_RUS.h>
 #include <Adafruit_NeoPixel.h>
 
-#define STRIP_PIN 3
 //Set the communication address of I2C to 0x27, display 16 characters every line, two lines in total
 LCD_1602_RUS mylcd(0x27, 16, 2);
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, STRIP_PIN, NEO_GRB + NEO_KHZ800);
-
 
 //set ports of two servos to digital 9 && 10c.lf $$
-Servo servo_10;
+Servo barrierServo;
 
 void printWelcome();
 void checkMotionLight();
-void checkRain();
 void switchFan();
 void trafficLight();
 void checkSerialInput();
 void switchNightLight();
 void printDebugMessage(String message);
-void tryClearScreen();
-void blinkLeds();
-void blinkOff();
-void tryPrintWelcome();
 void printAnalogSensor(int sensorPin, String message);
 void colorWipe(uint32_t c);
 
-volatile int infrar;
-String led2;
-volatile int light;
-volatile unsigned long startBlinking;
-volatile bool isLedsOn = false;
-volatile bool isFanPushed = false;
-volatile bool debugEnabled = false;
-volatile int val;
+int infrar;
+int light;
+bool isFanPushed = false;
+bool debugEnabled = false;
+int val;
 
-volatile bool isNightLightPushed = false;
-volatile bool isNightLightOn = false;
-volatile bool isTrafficButtonPushed = false;
-volatile bool isTrafficEnabled = false;
-volatile bool isGreenNeedWork = false;
-volatile bool isGreenOn = false;
-volatile bool isYellowOn = false;
-volatile bool isRedOn = false;
-volatile unsigned long greenBlinkStart = 0;
-volatile unsigned long yellowBlinkStart = 0;
-volatile unsigned long redBlinkStart = 0;
-volatile unsigned long nightLightStart = 0;
-unsigned long nightLightTime = 600L * 60 * 120;
-// unsigned long nightLightTime = 3000;
+bool isNightLightPushed = false;
+bool isNightLightOn = false;
+bool isTrafficButtonPushed = false;
+bool isTrafficEnabled = false;
+bool isGreenNeedWork = false;
+bool isGreenOn = false;
+bool isYellowOn = false;
+bool isRedOn = false;
+unsigned long greenBlinkStart = 0;
+unsigned long yellowBlinkStart = 0;
+unsigned long redBlinkStart = 0;
+unsigned long nightLightStart = 0;
+unsigned long nightLightTime = 600L * 60 * 120; // 600L * 60 * 120 is about 2 hours
 
-int windowServoPin = 2;
-//int yellowLedPin = 3;
-int redPin = 4;
-int yellowPin = 5;
-int greenPin = 6;
-int fanPin = 7;
-int fanPin2 = 8;
-int relayPin = 9;
+//digital pins
+const int BARRIES_SERVO_PIN = 2;
+const int STRIP_PIN = 3;
+const int RED_PIN = 4;
+const int YELLOW_PIN = 5;
+const int GREEN_PIN = 6;
+const int FAN_PIN = 7;
+const int FAN_PIN_2 = 8;
+const int RELAY_PIN = 9;
+const int NIGHT_LIGHT_BUTTON_PIN = 10;
 
-int photocellPin = 1;
-int steamPin = 2;
-int leftButton = 3;
-int rightButton = 4;
-int nightLightButton = 10;
-int motionPin = 5;
+//analog pins
+const int PHOTOCELL_PIN = 1;
+const int TRAFFIC_BUTTON_PIN = 3;
+const int FAN_BUTTON_PIN = 4;
+const int MOTION_PIN = 5;
 
-volatile bool isRain = false;
-volatile bool isGas = false;
 String EMPTY = "                ";
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
@@ -81,26 +67,24 @@ void setup() {
   mylcd.backlight();
   printWelcome();
 
-  servo_10.attach(windowServoPin);
-  servo_10.write(180);
+  barrierServo.attach(BARRIES_SERVO_PIN);
+  barrierServo.write(180);
 
-  pinMode(nightLightButton, INPUT);
+  pinMode(NIGHT_LIGHT_BUTTON_PIN, INPUT);
 
-  pinMode(relayPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(yellowPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(fanPin, OUTPUT);
-  pinMode(fanPin2, OUTPUT);
-//  pinMode(yellowLedPin, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
+  pinMode(FAN_PIN_2, OUTPUT);
 
-  digitalWrite(fanPin, LOW);
-  digitalWrite(fanPin2, LOW);
+  digitalWrite(FAN_PIN, LOW);
+  digitalWrite(FAN_PIN_2, LOW);
 }
 
 void loop() {
   checkMotionLight();
-  checkRain();
   switchFan();
   trafficLight();
   checkSerialInput();
@@ -108,15 +92,15 @@ void loop() {
 }
 
 void switchNightLight() {
-  if (digitalRead(nightLightButton) == LOW && !isNightLightPushed) {
+  if (digitalRead(NIGHT_LIGHT_BUTTON_PIN) == LOW && !isNightLightPushed) {
     isNightLightPushed = true;
     if (isNightLightOn) {
       printDebugMessage("Night light: OFF");
-      digitalWrite(relayPin, LOW);
+      digitalWrite(RELAY_PIN, LOW);
       isNightLightOn = false;
       mylcd.backlight();
     } else {
-      digitalWrite(relayPin, HIGH);
+      digitalWrite(RELAY_PIN, HIGH);
       nightLightStart = millis();
       Serial.print("Night light ON in ");
       Serial.println(nightLightStart);
@@ -124,23 +108,23 @@ void switchNightLight() {
       mylcd.noBacklight();
     }
   }
-  if (digitalRead(nightLightButton) != LOW && isNightLightPushed) {
+  if (digitalRead(NIGHT_LIGHT_BUTTON_PIN) != LOW && isNightLightPushed) {
     isNightLightPushed = false;
   }
-  if (isNightLightOn && millis() - nightLightStart > nightLightTime and digitalRead(relayPin) != LOW) {
+  if (isNightLightOn && millis() - nightLightStart > nightLightTime and digitalRead(RELAY_PIN) != LOW) {
     Serial.print("Night light auto OFF in ");
     Serial.println(millis());
     // isNightLightOn = false;
-    digitalWrite(relayPin, LOW);
+    digitalWrite(RELAY_PIN, LOW);
   }
 
 }
 
 void checkMotionLight() {
   if (!isTrafficEnabled) {
-    light = analogRead(photocellPin);
+    light = analogRead(PHOTOCELL_PIN);
     if (light < 300) {
-      infrar = analogRead(motionPin);
+      infrar = analogRead(MOTION_PIN);
       if (infrar > 200) {
        colorWipe(strip.Color(0, 0, 255)); // Blue
       } else {
@@ -159,73 +143,27 @@ void colorWipe(uint32_t c) {
   }
 }
 
-void checkRain() {
-  if (analogRead(steamPin) > 800 && !isRain) {
-    tryClearScreen();
-    mylcd.setCursor(5, 0);
-    mylcd.print("ДОЖДЬ!");
-    if (!isGas) {
-      servo_10.write(0);
-    }
-    isRain = true;
-  } else if (analogRead(steamPin) < 100 && isRain) {
-    isRain = false;
-    if (!isGas) {
-      servo_10.write(180);
-    }
-    tryPrintWelcome();
-  }
-}
-
-void blinkLeds() {
-  int valLeds = (((millis() - startBlinking) / 500) % 2);
-  if (valLeds == 0 && !isLedsOn) {
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(yellowPin, HIGH);
-    digitalWrite(redPin, HIGH);
-//    digitalWrite(yellowLedPin, HIGH);
-    mylcd.backlight();
-    isLedsOn = true;
-  } else if (valLeds == 1 && isLedsOn) {
-    digitalWrite(greenPin, LOW);
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(redPin, LOW);
-//    digitalWrite(yellowLedPin, LOW);
-    mylcd.noBacklight();
-    isLedsOn = false;
-  }
-}
-
-void blinkOff() {
-  digitalWrite(greenPin, LOW);
-  digitalWrite(yellowPin, LOW);
-  digitalWrite(redPin, LOW);
-//  digitalWrite(yellowLedPin, LOW);
-  mylcd.backlight();
-  isLedsOn = false;
-}
-
 void switchFan() {
-  if (analogRead(rightButton) < 100 && !isFanPushed) {
+  if (analogRead(FAN_BUTTON_PIN) < 100 && !isFanPushed) {
     isFanPushed = true;
-    if (digitalRead(fanPin2) == 1) {
-      digitalWrite(fanPin2, LOW);
+    if (digitalRead(FAN_PIN_2) == 1) {
+      digitalWrite(FAN_PIN_2, LOW);
     } else {
-      digitalWrite(fanPin2, HIGH);
+      digitalWrite(FAN_PIN_2, HIGH);
     }
   }
-  if (analogRead(rightButton) > 100 && isFanPushed) {
+  if (analogRead(FAN_BUTTON_PIN) > 100 && isFanPushed) {
     isFanPushed = false;
   }
 }
 
 void blinkGreen() {
   int valG = ((millis() - greenBlinkStart) / 3000) % 2;
-  if (valG == 0 && isGreenOn && digitalRead(greenPin) == LOW) {
-    digitalWrite(greenPin, HIGH);
+  if (valG == 0 && isGreenOn && digitalRead(GREEN_PIN) == LOW) {
+    digitalWrite(GREEN_PIN, HIGH);
     colorWipe(strip.Color(0, 100, 0));
-  } else if (valG > 0 && isGreenOn && digitalRead(greenPin) == HIGH) {
-    digitalWrite(greenPin, LOW);
+  } else if (valG > 0 && isGreenOn && digitalRead(GREEN_PIN) == HIGH) {
+    digitalWrite(GREEN_PIN, LOW);
     yellowBlinkStart = millis();
     isGreenOn = false;
     isYellowOn = true;
@@ -234,11 +172,11 @@ void blinkGreen() {
 
 void blinkYellow() {
   int valY = ((millis() - yellowBlinkStart) / 3000) % 2;
-  if (valY == 0 && isYellowOn && digitalRead(yellowPin) == LOW) {
-    digitalWrite(yellowPin, HIGH);
+  if (valY == 0 && isYellowOn && digitalRead(YELLOW_PIN) == LOW) {
+    digitalWrite(YELLOW_PIN, HIGH);
     colorWipe(strip.Color(100, 25, 0));
-  } else if (valY > 0 && isYellowOn && digitalRead(yellowPin) == HIGH) {
-    digitalWrite(yellowPin, LOW);
+  } else if (valY > 0 && isYellowOn && digitalRead(YELLOW_PIN) == HIGH) {
+    digitalWrite(YELLOW_PIN, LOW);
     redBlinkStart = millis();
     isYellowOn = false;
     isRedOn = true;
@@ -247,11 +185,11 @@ void blinkYellow() {
 
 void blinkRed() {
   int valR = ((millis() - redBlinkStart) / 3000) % 2;
-  if (valR == 0 && isRedOn && digitalRead(redPin) == LOW) {
-    digitalWrite(redPin, HIGH);
+  if (valR == 0 && isRedOn && digitalRead(RED_PIN) == LOW) {
+    digitalWrite(RED_PIN, HIGH);
     colorWipe(strip.Color(100, 0, 0));
-  } else if (valR > 0 && isRedOn && digitalRead(redPin) == HIGH) {
-    digitalWrite(redPin, LOW);
+  } else if (valR > 0 && isRedOn && digitalRead(RED_PIN) == HIGH) {
+    digitalWrite(RED_PIN, LOW);
     greenBlinkStart = millis();
     isRedOn = false;
     isGreenOn = true;
@@ -259,7 +197,7 @@ void blinkRed() {
 }
 
 void trafficLight() {
-  if (analogRead(leftButton) < 100 && !isTrafficButtonPushed) {
+  if (analogRead(TRAFFIC_BUTTON_PIN) < 100 && !isTrafficButtonPushed) {
     isTrafficButtonPushed = true;
     if (!isTrafficEnabled) {
       printDebugMessage("Traffic light: ON");
@@ -270,9 +208,9 @@ void trafficLight() {
       isRedOn = false;
     } else {
       printDebugMessage("Traffic light: OFF");
-      digitalWrite(greenPin, LOW);
-      digitalWrite(yellowPin, LOW);
-      digitalWrite(redPin, LOW);
+      digitalWrite(GREEN_PIN, LOW);
+      digitalWrite(YELLOW_PIN, LOW);
+      digitalWrite(RED_PIN, LOW);
       colorWipe(0);
       isGreenOn = false;
       isYellowOn = false;
@@ -287,7 +225,7 @@ void trafficLight() {
     blinkRed();
   }
 
-  if (analogRead(leftButton) > 100 && isTrafficButtonPushed) {
+  if (analogRead(TRAFFIC_BUTTON_PIN) > 100 && isTrafficButtonPushed) {
     isTrafficButtonPushed = false;
   }
 }
@@ -302,23 +240,10 @@ void printWelcome() {
 
 void printSensors() {
   Serial.println("Analog sensors:");
-  printAnalogSensor(photocellPin, "  Photocell sensor");
-  printAnalogSensor(steamPin, "  Humidity sensor");
-  printAnalogSensor(leftButton, "  Left button");
-  printAnalogSensor(rightButton, "  Right button");
-  printAnalogSensor(motionPin, "  Motion sensor");
-}
-
-void tryClearScreen() {
-  if (!isRain) {
-    mylcd.clear();
-  }
-}
-
-void tryPrintWelcome() {
-  if (!isRain) {
-    printWelcome();
-  }
+  printAnalogSensor(PHOTOCELL_PIN, "  Photocell sensor");
+  printAnalogSensor(TRAFFIC_BUTTON_PIN, "  Left button");
+  printAnalogSensor(FAN_BUTTON_PIN, "  Right button");
+  printAnalogSensor(MOTION_PIN, "  Motion sensor");
 }
 
 void printAnalogSensor(int sensorPin, String message) {
